fbe8e78fe196149ee7dc1af2be3cf3cb1e4e635a
leviportugal <102754345+leviportugal@users.noreply.github.com>
2022-03-31 13:45:24 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var quadrado
+
+
+
+function setup() {
+  createCanvas(400,400);
+
+  quadrado = createSprite (200,200,20,20);
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+drawSprites ();
+}
+
+
+
+
